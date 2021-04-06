f0d930c3dbc5dc763718ea54a26e79346d26e08f
Debora Durek <debora.durek@gmail.com>
2021-04-06 12:34:12 -0500

    feat: Create AddUser component to hold form input

---
+++ src/AddUser.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class AddUser extends Component {
+  static propTypes = {};
+
+  state = {
+    gameUser: {
+      firstName: '',
+      lastName: '',
+      username: '',
+    },
+  };
+  render() {
+    return (
+      <div>
+        <h1>New User</h1>
+        <form onSubmit={this.addUser}>
+          <input type="text" name="firstName" placeholder="Enter First Name" />
+          <input type="text" name="lastName" placeholder="Enter Last Name" />
+          <input type="text" name="username" placeholder="Enter Username" />
+          <button>Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddUser;
