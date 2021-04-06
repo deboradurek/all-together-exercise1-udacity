4ec6b579406a0c0921982113a303719877c1eff9
Debora Durek <debora.durek@gmail.com>
2021-04-06 12:01:57 -0500

    feat: Create User component for single user

---
+++ src/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const User = (props) => (
+  <li className="user">
+    <p></p>
+    <p>Number of games played: </p>
+  </li>
+);
+
+User.propTypes = {};
+
+export default User;
