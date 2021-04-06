c8c2f19923cfb3d882c29afde7bbc027c911d896
Debora Durek <debora.durek@gmail.com>
2021-04-06 12:17:32 -0500

    feat: Transform App component to class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './UserList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      <div></div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">React Nanodegree - Coding Practice by Udacity</h2>
+        </header>
+        <div>
+          <UserList />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
