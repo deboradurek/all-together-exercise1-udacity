6c4f9a11d08bd64f17a3b1624b63bc09bca6cc9d
Debora Durek <debora.durek@gmail.com>
2021-04-09 14:57:28 -0500

    feat: Add display for score panel

--- src/User.js
+++ src/User.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const User = (props) => (
   <li className="user">
     <p>{props.gameUser.username}</p>
-    <p>Number of games played: {props.showGamesPlayed}</p>
+    <p>Number of games played: {props.showGamesPlayed ? props.gameUser.numGamesPlayed : '*'}</p>
   </li>
 );
 
--- src/UserList.js
+++ src/UserList.js
@@ -11,13 +11,28 @@ class UserList extends Component {
     showGamesPlayed: true,
   };
 
+  gamesPlayedPanel = () => {
+    this.setState((currentState) => ({
+      showGamesPlayed: !currentState.showGamesPlayed,
+    }));
+  };
+
   render() {
     const { gameUsers } = this.props;
     const showGamesPlayed = this.state.showGamesPlayed;
 
+    const gamesPlayedToggleButton = (
+      <div>
+        <button className="smallButton" onClick={this.gamesPlayedPanel}>
+          {showGamesPlayed ? 'Hide' : 'Show'} the Number of Games Played
+        </button>
+      </div>
+    );
+
     return (
       <div>
         <h3>List Of Users</h3>
+        {gameUsers && gameUsers.length > 0 ? gamesPlayedToggleButton : ''}
         <ol>
           {gameUsers.map((gameUser) => (
             <User key={gameUser.username} gameUser={gameUser} showGamesPlayed={showGamesPlayed} />
